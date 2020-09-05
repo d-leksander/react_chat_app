5de60252eaa3c520850a0d6a234666d0592e4794
d-leksander <leksanderdarek@gmail.com>
2020-09-06 00:13:12 +0200

    added client components

---
+++ client/src/components/TextContainer/TextContainer.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+
+import "./TextContainer.css";
+
+const TextContainer = ({ users }) => (
+    <div className="textContainer">
+        <div>
+            <h1>
+                React Chat Application{" "}
+                <span role="img" aria-label="emoji">
+          💬
+        </span>
+            </h1>
+            <h2>
+                Welcome in chat application. {" "}
+                <span role="img" aria-label="emoji">
+          💬
+        </span>
+            </h2>
+            <h2>
+                Active users:{" "}
+            </h2>
+        </div>
+    </div>
+);
+
+export default TextContainer;
